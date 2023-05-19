ba203d08c65eee89f448cb716b3675afd50e5811
shazaaly <shaza.aly@gmail.com>
2023-05-19 23:20:46 +0200

    check access

---
+++ check_access.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+
+/* get the full path of a command entered by the user - check if the command is executable */
+/* Args: line from user entered by getline and path list */
+/* Return: full path of the command if it is found and executable, NULL otherwise */
+
+char *check_access(char *line, list_path *path_list)
+{
+	char *token;
+	char *full_path = NULL;
+	size_t token_len;
+    size_t path_len;
+	struct stat file_stat;
+	list_path *current = path_list; /*pointer to linkedlist*/
+
+	token = strtok(line, " \t\n");
+	if (token == NULL || path_list == NULL)
+	{
+		return (NULL);
+	}
+
+	/*search path for corresponding excutable*/
+	while (current != NULL)
+	{
+		path_len = _strlen(current->path);
+		token_len = _strlen(token);
+				/* create the full path*/
+		full_path = malloc(sizeof(char) * (path_len + token_len + 2));
+		if (full_path == NULL)
+		{
+			return NULL;
+		}
+		/*fill full path with path + '/' + token by coppying into full_path*/
+		_strcpy(full_path, current->path); /*path went*/
+		_strcat(full_path, "/");
+		_strcat(full_path, token);
+		full_path[path_len] = '\0';
+		/*end of full path concat*/
+
+		/*use stat to check stat() to check if a file exists and is executable*/
+		if (stat(full_path, &file_stat) == 0 && file_stat.st_mode & S_IXUSR)
+		{
+			return (full_path);
+		}
+		free(full_path); /*malloced previously*/
+
+		current = current->next; /*move to next dir*/
+
+
+	}
+	return (NULL);
+}
+
+
--- shell.h
+++ shell.h
@@ -36,5 +36,8 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_getenv(const char *name);
+void print_env(char ** line);
+
 
 #endif
+
